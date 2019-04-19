12fb13c37fc00dea4f2d075764bc30b6e2e288a6
Hiker <172004133@qq.com>
2019-04-19 17:46:12 +0800

    add selection sort

--- sorting-algorithm/sort.js
+++ sorting-algorithm/sort.js
@@ -20,15 +20,35 @@ function bubbleSort (arr) {
 					arr[j + 1] = temp;
 				}
 		 }
+   	 console.log(arr);
 	 }
 }
+
+/**
+ * 选择排序
+ * @param arr
+ */
+function selectionSort (arr) {
+	let length = arr.length;
+	for (let i = 0; i <= length - 2; i++) {
+		for (let j = i + 1; j <= length - 1; j++) {
+			if (arr[j] < arr[i]) {
+				let temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		console.log(arr);
+	}
+}
+
 
 /**
  * 测试排序方法
  */
 function testSort () {
 	let arr = [10, 8, 3, 2, 2, 4, 9, 5, 4, 3];
-	bubbleSort(arr);
+	selectionSort(arr);
 	console.log(arr);
 }
 testSort();
