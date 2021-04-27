3ff9678c647f2760c0ef4c3dc7a67dda6cabca44
yide <172004133@qq.com>
2021-04-27 13:59:27 +0800

    update: resolve promise test and program

--- function/promise.js
+++ function/promise.js
@@ -1,131 +1,135 @@
-const PENDING = "pending";
-const REJECT = "reject";
-const RESOLVE = "resolve";
+const PENDING = 'pending'
+const REJECT = 'reject'
+const RESOLVE = 'resolve'
 
 class MyPromise {
   // 构造函数接收处理方法
   constructor(executor) {
-    this._resolveQuene = [];
-    this._rejectQuene = [];
-    this._status = PENDING;
-    this._value = undefined;
+    this._resolveQuene = []
+    this._rejectQuene = []
+    this._status = PENDING
+    this._value = undefined
     // 构造函数里面创建resolve执行方法
-    let _resolve = val => {
+    let _resolve = (val) => {
       const run = () => {
-        if (this._status !== PENDING) return;
-        this._status = RESOLVE;
+        if (this._status !== PENDING) return
+        this._status = RESOLVE
         while (this._resolveQuene.length > 0) {
-          const callback = this._resolveQuene.shift();
-          callback(val);
-          this._value = val;
+          const callback = this._resolveQuene.shift()
+          callback(val)
+          this._value = val
         }
-      };
-      setTimeout(run);
-    };
+      }
+      setTimeout(run)
+    }
     // 创建rejec方法
-    let _reject = val => {
+    let _reject = (val) => {
       const run = () => {
-        if (this._status !== PENDING) return;
-        this._status = REJECT;
+        if (this._status !== PENDING) return
+        this._status = REJECT
         while (this._rejectQuene.length > 0) {
-          const callback = this._rejectQuene.shift();
-          callback(val);
-          this._value = val;
+          const callback = this._rejectQuene.shift()
+          callback(val)
+          this._value = val
         }
-      };
-      setTimeout(run);
-    };
+      }
+      setTimeout(run)
+    }
 
-    executor(_resolve, _reject);
+    executor(_resolve, _reject)
   }
 
   then(resolveFn, rejectFn) {
     // 防止resolve和reject不是方法，导致链式调用失败，所以进行校验是否为方法
-    typeof resolveFn !== "function" ? (resolveFn = value => value) : null;
-    typeof rejectFn !== "function"
-      ? (rejectFn = reason => {
-          throw new Error(reason instanceof Error ? reason.message : reason);
+    typeof resolveFn !== 'function' ? (resolveFn = (value) => value) : null
+    typeof rejectFn !== 'function'
+      ? (rejectFn = (reason) => {
+          throw new Error(reason instanceof Error ? reason.message : reason)
         })
-      : null;
+      : null
 
     // 为了.then的链式调用
     return new MyPromise((resolve, reject) => {
-      const fulfilledFn = value => {
+      const fulfilledFn = (value) => {
         try {
-          let x = resolveFn(value);
-          x instanceof MyPromise ? x.then(resolve, reject) : resolve(x);
+          let x = resolveFn(value)
+          x instanceof MyPromise ? x.then(resolve, reject) : resolve(x)
         } catch (error) {
-          reject(error);
+          reject(error)
         }
-      };
-      const rejectedFn = value => {
+      }
+      const rejectedFn = (value) => {
         try {
-          let y = rejectFn(value);
-          y instanceof MyPromise ? y.then(resolve, reject) : resolve(y);
+          let y = rejectFn(value)
+          y instanceof MyPromise ? y.then(resolve, reject) : resolve(y)
         } catch (error) {
-          reject(error);
+          reject(error)
         }
-      };
+      }
       // 切换状态，如果已经是reject 或者resolve直接执行then方法
       switch (this._status) {
         case PENDING:
-          this._resolveQuene.push(fulfilledFn);
-          this._rejectQuene.push(rejectedFn);
-          break;
+          this._resolveQuene.push(fulfilledFn)
+          this._rejectQuene.push(rejectedFn)
+          break
         case RESOLVE:
-          fulfilledFn(this._value);
-          break;
+          fulfilledFn(this._value)
+          break
         case REJECT:
-          rejectedFn(this._value);
+          rejectedFn(this._value)
       }
-    });
+    })
   }
   catch(rejectedFn) {
-    return this.then(undefined, rejectedFn);
+    return this.then(undefined, rejectedFn)
   }
   finally(callback) {
     return this.then(
-      value => MyPromise.resolve(callback()).then(() => value),
-      reason =>
+      (value) => MyPromise.resolve(callback()).then(() => value),
+      (reason) =>
         MyPromise.reject(callback()).then(() => {
-          throw reason;
+          throw reason
         })
-    );
+    )
   }
   static resolve(value) {
-    if (value instanceof MyPromise) return value;
-    return new MyPromise(resolve => resolve(value));
+    if (value instanceof MyPromise) return value
+    return new MyPromise((resolve) => resolve(value))
   }
   static reject(value) {
-    return new MyPromise((resolve, reject) => reject(value));
+    return new MyPromise((resolve, reject) => reject(value))
   }
   static all(arr) {
-    let index = 0;
-    let result = [];
+    let index = 0
+    let result = []
     return new MyPromise((resolve, reject) => {
       arr.forEach((p, i) => {
         MyPromise.resolve(p).then(
-          val => {
-            index++;
-            result[i] = val;
+          (val) => {
+            index++
+            result[i] = val
             if (index === arr.length) {
-              resolve(result);
+              resolve(result)
             }
           },
-          err => {
-            reject(err);
+          (err) => {
+            reject(err)
           }
-        );
-      });
-    });
+        )
+      })
+    })
   }
 }
 
-// const myPromise = new MyPromise((resolve, reject) => {
-//   // setTimeout(function() {
-//   resolve(1);
-//   // }, 1000);
-// });
+const myPromise = new MyPromise((resolve, reject) => {
+  setTimeout(function () {
+    resolve(1)
+  }, 1000)
+})
+console.log('start promise')
+myPromise.then(res => {
+  console.log(res)
+})
 // myPromise
 //   .then(res => {
 //     console.log(res);
@@ -150,17 +154,17 @@ class MyPromise {
 //   );
 
 // 测试all
-const p1 = new MyPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(1);
-  }, 100);
-});
-const p2 = new MyPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(21);
-  }, 1001);
-});
+// const p1 = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(1);
+//   }, 100);
+// });
+// const p2 = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(21);
+//   }, 1001);
+// });
 
-MyPromise.all([p1, p2]).then(res => {
-  console.log(res);
-});
+// MyPromise.all([p1, p2]).then(res => {
+//   console.log(res);
+// });
