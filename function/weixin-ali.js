7d80054368588a5106c7dc36492a6bd0586021a2
yide <172004133@qq.com>
2021-05-27 08:56:11 +0800

    add: javascript4 version

--- function/weixin-ali.js
+++ function/weixin-ali.js
@@ -1,34 +1,32 @@
 function getPlatform() {
   if (/MicroMessenger/.test(window.navigator.userAgent)) {
-    return "weixin";
+    return 'weixin'
   } else if (/AlipayClient/.test(window.navigator.userAgent)) {
-    return "ali";
+    return 'ali'
   } else {
-    return "other";
+    return 'other'
   }
 }
 function getLocation() {
-  const platform = getPlatform();
-  if (platform === "weixin") {
-    WeixinJSBridge.invoke("geoLocation",{
-    },
-    function(e){
+  const platform = getPlatform()
+  if (platform === 'weixin') {
+    WeixinJSBridge.invoke('geoLocation', {}, function (e) {
       alert(e.err_msg)
     })
-  } else if (platform === "ali") {
+  } else if (platform === 'ali') {
     getLocationAli(function () {
       AlipayJSBridge.call(
-        "getCurrentLocation",
-        { requestType: 2, bizType: "didi" },
+        'getCurrentLocation',
+        { requestType: 2, bizType: 'didi' },
         function (result) {
           if (result.error) {
-            alert(result.errorMessage);
-            return;
+            alert(result.errorMessage)
+            return
           }
-          alert(JSON.stringify(result));
+          alert(JSON.stringify(result))
         }
-      );
-    });
+      )
+    })
   } else {
   }
 }
@@ -36,9 +34,9 @@ function getLocation() {
 function getLocationAli(callback) {
   // 如果jsbridge已经注入则直接调用
   if (window.AlipayJSBridge) {
-    callback && callback();
+    callback && callback()
   } else {
     // 如果没有注入则监听注入的事件
-    document.addEventListener("AlipayJSBridgeReady", callback, false);
+    document.addEventListener('AlipayJSBridgeReady', callback, false)
   }
 }
